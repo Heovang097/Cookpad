74656d41fa071b2dea9d87fb67ce5180cb534961
thanhan17 <andoan1720@gmail.com>
2020-12-31 10:53:36 +0700

    Update YouFragment

--- app/src/main/java/com/example/cookpad/fragments/YouFragment.java
+++ app/src/main/java/com/example/cookpad/fragments/YouFragment.java
@@ -1,21 +1,26 @@
 package com.example.cookpad.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.cookpad.R;
 import com.example.cookpad.fragments.you.MyImageFragment;
 import com.example.cookpad.fragments.you.MyRecipeFragment;
 import com.example.cookpad.fragments.you.SavedRecipeFragment;
+import com.example.cookpad.fragments.you.UpdateActivity;
 import com.google.android.material.tabs.TabLayout;
 
 import java.util.ArrayList;
@@ -24,18 +29,42 @@ import java.util.List;
 public class YouFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
         setRetainInstance(true);
+        setHasOptionsMenu(true);
+        super.onCreate(savedInstanceState);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_you, container, false);
-        Toolbar toolbar = (Toolbar) (Toolbar) view.findViewById(R.id.toolbarYou);
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbarYou);
         toolbar.setLogo(R.drawable.avatar);
         toolbar.setTitle(" Đoàn Văn Thanh An");
-
+        toolbar.inflateMenu(R.menu.menu_you);
+        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.YourPage:
+                        Toast.makeText(getActivity(), "Your page", Toast.LENGTH_SHORT).show();
+                        YouFragment your = new YouFragment();
+                        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                        fragmentTransaction.replace(R.id.YouF, your);
+                        fragmentTransaction.addToBackStack(null);
+                        fragmentTransaction.commit();
+                        return true;
+                    case R.id.UpdateInfo:
+                        Toast.makeText(getActivity(), "Update", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(getActivity(), UpdateActivity.class);
+                        startActivity(intent);
+                        return true;
+                }
+                Toast.makeText(getActivity(), "This is my Toast message!", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
         ViewPager viewPager = (ViewPager) view.findViewById(R.id.view_pagerYou);
         setupViewPager(viewPager);
         // Set Tabs inside Toolbar
@@ -43,6 +72,7 @@ public class YouFragment extends Fragment {
         tabs.setupWithViewPager(viewPager);
         return view;
     }
+
     private void setupViewPager(ViewPager viewPager) {
         Adapter adapter = new Adapter(getChildFragmentManager());
         adapter.addFragment(new SavedRecipeFragment(), "Món đã lưu");
@@ -50,6 +80,7 @@ public class YouFragment extends Fragment {
         adapter.addFragment(new MyImageFragment(), "Hình thực hành");
         viewPager.setAdapter(adapter);
     }
+
     static class Adapter extends FragmentPagerAdapter {
         private final List<Fragment> mFragmentList = new ArrayList<>();
         private final List<String> mFragmentTitleList = new ArrayList<>();
@@ -72,7 +103,6 @@ public class YouFragment extends Fragment {
             mFragmentList.add(fragment);
             mFragmentTitleList.add(title);
         }
-
         @Override
         public CharSequence getPageTitle(int position) {
             return mFragmentTitleList.get(position);
