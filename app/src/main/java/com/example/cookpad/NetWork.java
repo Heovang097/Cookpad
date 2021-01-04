8a1e0206bfb76e5f9f7614c1aeead5e602de44de
thanhan17 <andoan1720@gmail.com>
2021-01-04 22:26:08 +0700

    Update YouFragment

--- app/src/main/java/com/example/cookpad/NetWork.java
+++ app/src/main/java/com/example/cookpad/NetWork.java
@@ -5,7 +5,7 @@ public class NetWork {
     private String SERVER ;
 
     private NetWork(){
-        SERVER="192.168.1.8:8999";
+        SERVER="192.168.1.9:8000";
     }
     public static NetWork getNetworkInfoHolder(){
         if(obj==null){
